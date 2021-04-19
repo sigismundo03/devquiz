bdf4afd90fbbfbe2cc6c02e25a0c3108d0f4357f
Sigismundo4321 <sigismundo4321@gmail.com>
2021-04-19 10:05:52 -0300

    alterando o nome e crinado splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
 
+import 'core/app_widget.dart';
+
 void main() {
-  runApp(MyApp());
+  runApp(AppWidget());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Quiz Dev',
-      theme: ThemeData(
-    
-      ),
-      home: Container(),
-    );
-  }
-}
+
 
