66676b21d29cbbf2b1f33cc8922efc92d75cbf87
Sigismundo4321 <sigismundo4321@gmail.com>
2021-04-19 19:55:44 -0300

    primeira parte do app do appbar

--- lib/home/widgets/apparbar/app_bar_widget.dart
+++ lib/home/widgets/apparbar/app_bar_widget.dart
@@ -1,5 +1,6 @@
 import 'package:DevQuiz/core/app_gradients.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/home/widgets/scorecard/score_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppBarWidget extends PreferredSize {
@@ -8,37 +9,47 @@ class AppBarWidget extends PreferredSize {
             preferredSize: Size.fromHeight(250),
             child: Container(
               height: 250,
-              decoration: BoxDecoration(gradient: AppGradients.linear),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text.rich(TextSpan(
-                     text: "Olá, ",
-                     style: AppTextStyles.title,
-                     children: [
-                       TextSpan(
-                        text: "Sigis Dev",
-                        style: AppTextStyles.titleBold,
-                    ),
-                     ]       
-                     ),
-                    ),
-                    
-                    Container(
-                      width: 58,
-                      height: 58,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              'https://avatars.githubusercontent.com/u/42527356?v=4'),
+              child: Stack(
+                children: [
+                  Container(
+                    height: 161,
+                    decoration: BoxDecoration(gradient: AppGradients.linear),
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text.rich(
+                          TextSpan(
+                              text: "Olá, ",
+                              style: AppTextStyles.title,
+                              children: [
+                                TextSpan(
+                                  text: "Sigis Dev",
+                                  style: AppTextStyles.titleBold,
+                                ),
+                              ]),
                         ),
-                      ),
+                        Container(
+                          width: 58,
+                          height: 58,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            image: DecorationImage(
+                              image: NetworkImage(
+                                  'https://avatars.githubusercontent.com/u/42527356?v=4'),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                  Align(
+                    alignment: Alignment(0.0,1.0),
+                    child: ScoreCardWidget(
+
+                    ),
+                  ),
+                ],
               ),
             ));
 }
--- lib/home/widgets/scorecard/score_card_widget.dart
+++ lib/home/widgets/scorecard/score_card_widget.dart
@@ -1,25 +1,57 @@
+
+
+import 'package:DevQuiz/home/widgets/chart/chart_widget.dart';
+import 'package:DevQuiz/core/app_colors.dart';
+import 'package:DevQuiz/core/app_text_styles.dart';
+
+
 import 'package:flutter/material.dart';
 
 class ScoreCardWidget extends StatelessWidget {
+  ScoreCardWidget({Key ? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Container(
-            width: 80,
-            height: 80,
-            color: Colors.red,
-          ),
-          Column(
-            children: [
-              Text("Vamos Começa"),
-              Text("Comple os desafios e avance em conhecimento "),
-            ],
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      child: Container(
+        height: 136,
+        decoration: BoxDecoration(
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 1,
+              child: ChartWidget(),
+            ),
+             Expanded(
+               flex: 3,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Vamos Começa",
+                      style: AppTextStyles.heading,  
+                    ),
+                    Text(
+                      "Comple os desafios e avance em conhecimento",
+                      style: AppTextStyles.body,   
+                      
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        
       ),
-      
     );
   }
 }
