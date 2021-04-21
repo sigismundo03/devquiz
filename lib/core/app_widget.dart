bd9cbadf51b1df6344e8d437a84360430f21caaa
Sigismundo4321 <sigismundo4321@gmail.com>
2021-04-21 08:30:10 -0300

    finalizado appbar com quiz indicator

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,7 +1,7 @@
 
 
 import 'package:DevQuiz/challenge/challenge.dart';
-import 'package:DevQuiz/home/home_page.dart';
+// import 'package:DevQuiz/home/home_page.dart';
 // import 'package:DevQuiz/splash/splash_page.dart';
 
 import 'package:flutter/material.dart';
--- lib/home/widgets/quizcard/quiz_card_widget.dart
+++ lib/home/widgets/quizcard/quiz_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:DevQuiz/core/app_colors.dart';
 import 'package:DevQuiz/core/app_images.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -47,10 +48,8 @@ class QuizCardWidget extends StatelessWidget {
                 ),
                  Expanded(
                    flex: 2,
-                  child: LinearProgressIndicator(
-                    value: 0.4,
-                    backgroundColor: AppColors.chartSecondary,
-                    valueColor: AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
+                  child: ProgressIndicatorWidget(
+                    value: 0.3,
                   ),
                 ),
               ],
